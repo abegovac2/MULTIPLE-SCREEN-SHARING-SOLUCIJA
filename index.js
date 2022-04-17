8a5e64a8be56f10f815732cb71ebc368cac29632
abegovac2 <begovac5@gmail.com>
2022-04-17 17:44:10 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const sequelize = require("./database/database.js");
+const tokenAuth = require("./middleware/tokenAuth.js")
+
+const userRoute = require("./routes/userRoute.js");
+const meetManagement = require("./routes/meetManagementRoute.js");
+
+app.use(bodyParser.json());
+app.use(tokenAuth.auth);
+
+require('dotenv').config()
+
+app.use("/user", userRoute);
+app.use("/meets", meetManagement);
+
+
+sequelize
+  .sync({ forice: true })
+  .then(() => app.listen(process.env.PORT))
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = app;
