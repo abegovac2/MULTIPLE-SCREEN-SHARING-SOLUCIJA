9309b41c919aa5070e7a87ff9baaaf49e0ce9c8f
abegovac2 <begovac5@gmail.com>
2022-04-22 01:08:03 +0200

    refactor

--- app/controllers/meetManagementController.js
+++ app/controllers/meetManagementController.js
@@ -1,16 +1,17 @@
 const Meet = require("../models/meet.js");
 const routeWrappers = require("../utils/routeWrappers.js");
 const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
 const fs = require("fs");
 
 const createMeetController = (() => {
-
 	const formatMeetObj = (meet) => {
-		meet.passwordProtected = meetInfo.studentPassword || meetInfo.teacherPassword;
+		meet.passwordProtected =
+			meetInfo.studentPassword || meetInfo.teacherPassword;
 		delete meet.studentPassword;
 		delete meet.teacherPassword;
-		return meet
-	}
+		return meet;
+	};
 
 	const createMeet = async (req, res) => {
 		let meet = req.body;
@@ -20,45 +21,49 @@ const createMeetController = (() => {
 		meet.studentPassword = await bcrypt.hash(meet.studentPassword ?? "", 10);
 		meet.teacherPassword = await bcrypt.hash(meet.teacherPassword ?? "", 10);
 
-		try {
-			let newMeet = await Meet.create(meet);
-			res.send(201).send(newMeet);
-		} catch (e) {
-			res.status(400).send({ message: "Meet already exists!" });
-		}
-
-	}
+		let newMeet = await Meet.findOrCreate({
+			where: {
+				meetName: meetName,
+			},
+			defaults: newMeet,
+		});
+		if (newMeet.startTime != meet.startTime) res.send(201).send(newMeet);
+		else res.status(400).send({ message: "Meet already exists!" });
+	};
 
 	const getAllMeets = async (req, res) => {
-
 		let allMeets = await Meet.findAll({
-			attributes: ['meetName', 'subject', 'createdBy']
+			attributes: ["meetName", "subject", "createdBy"],
 		});
 		res.status(200).send({ allMeets: allMeets });
-
-	}
+	};
 
 	const getMeetInfo = async (req, res) => {
-
 		let { meetName } = req.body;
 		let meetInfo = await Meet.findOne({ where: { meetName: meetName } });
 		meetInfo = formatMeetObj(meetInfo);
 		res.status(200).send({ meetInfo: meetInfo });
+	};
 
-	}
-
-	const configurationCheck = async (file, passwordInput, passwordMeet, meet, res) => {
+	const configurationCheck = async (
+		file,
+		passwordInput,
+		passwordMeet,
+		meet,
+		res
+	) => {
 		let isValid = await bcrypt.compare(passwordInput, passwordMeet);
 		if (isValid) {
 			fs.readFile(`../setupData/${file}`, "utf8", (err, setup) => {
 				if (err)
-					throw err;//res.status(500).send({ message: "Internal server error" });
+					throw err; //res.status(500).send({ message: "Internal server error" });
 				else
-					res.status(200).send({ meet: formatMeetObj(meet), setup: JSON.parse(setup) });
-
+					res
+						.status(200)
+						.send({ meet: formatMeetObj(meet), setup: JSON.parse(setup) });
 			});
 		} else res.status(400).send({ message: "Invalid password" });
-	}
+	};
 
 	const enterMeet = async (req, res) => {
 		const { meetName, studentPassword, teacherPassword } = req.body;
@@ -66,39 +71,63 @@ const createMeetController = (() => {
 		let meet = await Meet.findOne({ where: { meetName: meetName } });
 
 		if (!meet) {
-			res.status(404).send({ message: `Meet named ${meetName} does not exist.` });
+			res
+				.status(404)
+				.send({ message: `Meet named ${meetName} does not exist.` });
 			return;
 		}
 
-		if (meet.endDate != null) {
-			res.status(409).send({ message: `Meet named ${meetName} has finished at ${meet.endDate}.` });
+		if (!meet.endDate) {
+			res.status(409).send({
+				message: `Meet named ${meetName} has finished at ${meet.endDate}.`,
+			});
 			return;
 		}
 
 		if (studentPassword)
-			configurationCheck('studentMeet', studentPassword, meet.studentPassword, meet, res);
+			configurationCheck(
+				"studentMeet",
+				studentPassword,
+				meet.studentPassword,
+				meet,
+				res
+			);
 		else if (teacherPassword)
-			configurationCheck('teacherMeet', teacherPassword, meet.teacherPassword, meet, res);
-		else throw "error";
-
-	}
+			configurationCheck(
+				"teacherMeet",
+				teacherPassword,
+				meet.teacherPassword,
+				meet,
+				res
+			);
+		else
+			res
+				.status(401)
+				.send({ message: "A password must be provided to enter the meet." });
+	};
 
 	const endMeet = async (req, res) => {
 		const { meetName, teacherPassword } = req.body;
 		let meet = await Meet.findOne({ where: { meetName: meetName } });
 
 		if (!meet) {
-			res.status(404).send({ message: `Meet named ${meetName} does not exist.` });
+			res
+				.status(404)
+				.send({ message: `Meet named ${meetName} does not exist.` });
 			return;
 		}
 
 		if (meet.endDate != null) {
-			res.status(409).send({ message: `Meet named ${meetName} has finished at ${meet.endDate}.` });
+			res.status(409).send({
+				message: `Meet named ${meetName} has finished at ${meet.endDate}.`,
+			});
 			return;
 		}
 
-		if (!await bcrypt.compare(teacherPassword, meet.teacherPassword)) {
-			res.status(400).send({ message: `Incorrect password for meet: ${meetName}` });
+		if (!(await bcrypt.compare(teacherPassword, meet.teacherPassword))) {
+			res
+				.status(400)
+				.send({ message: `Incorrect password for meet: ${meetName}` });
 			return;
 		}
 
@@ -106,19 +135,22 @@ const createMeetController = (() => {
 
 		await Meet.update(meet, {
 			where: {
-				id: meet.id
-			}
+				id: meet.id,
+			},
 		});
 
-	}
+		res.status(201).send({
+			message: `Meet ${meet.meetName} has finished at ${meet.endDate}.`,
+		});
+	};
 
-	return [
-		createMeet,
-		getAllMeets,
-		getMeetInfo,
-		enterMeet,
-		endMeet
-	].reduce((obj, curr) => ({ ...obj, [curr.name]: routeWrappers.serverErrorWrap(curr) }), {});
+	return [createMeet, getAllMeets, getMeetInfo, enterMeet, endMeet].reduce(
+		(obj, curr) => ({
+			...obj,
+			[curr.name]: routeWrappers.serverErrorWrap(curr),
+		}),
+		{}
+	);
 })();
 
 module.exports = createMeetController;
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -6,121 +6,128 @@ const { Op } = require("sequelize");
 const User = require("../models/user.js");
 
 const userController = (() => {
+  const userPropCheck = (user, res) => {
+    let result = !["userName", "email", "password"].every((el) => el in user);
+    if (result)
+      res.status(400).send({ message: "Invalid input data for route" });
+    return result;
+  };
 
-	const userPropCheck = (user, res) => {
-		let result = !['userName', 'email', 'password'].every(el => el in user);
-		if (result) res.status(400).send({ message: "Invalid input data for route" });
-		return result;
-	}
+  const findUserInDB = async (user, res) => {
+    let dbUser = await User.findOne({
+      where: {
+        userName: user.userName,
+        [Op.or]: {
+          email: user.email,
+        },
+      },
+    });
 
-	const findUserInDB = async (user, res) => {
-		let dbUser = await User.findOne({
-			where: {
-				userName: user.userName,
-				[Op.or]: {
-					email: user.email,
-				},
-			},
-		});
+    return (
+      dbUser ||
+      res
+        .status(404)
+        .send({ message: `Querried user, ${user.userName}, does not exist!` })
+    );
+  };
 
-		return dbUser || res.status(404).send({ message: `Querried user, ${user.userName}, does not exist!` });
-	}
+  const register = async (req, res) => {
+    let user = req.body;
+    if (userPropCheck(user, res)) return;
+    const hashPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashPassword;
+    try {
+      await User.create(user);
+      res.status(201).send({ userName: user.userName });
+    } catch (e) {
+      res
+        .status(409)
+        .send({
+          userName: `User with username: '${user.userName}' already exist!`,
+        });
+    }
+  };
 
-	const register = async (req, res) => {
-		let user = req.body;
-		if (userPropCheck(user, res)) return;
-		const hashPassword = await bcrypt.hash(user.password, 10);
-		user.password = hashPassword;
-		try {
-			await User.create(user);
-			res.status(201).send({ userName: user.userName, });
-		} catch (e) {
-			res.status(409).send({ userName: `User with username: '${user.userName}' already exist!`, });
-		}
-	}
+  const login = async (req, res) => {
+    let inUser = req.body;
 
-	const login = async (req, res) => {
-		let inUser = req.body;
+    if (
+      !(("userName" in inUser || "email" in inUser) && "password" in inUser)
+    ) {
+      res.status(400).send({ message: "Invalid input data for route" });
+      return;
+    }
+    let dbUser = findUserInDB(inUser, res);
+    if (dbUser) return;
 
-		if (!(('userName' in inUser || 'email' in inUser) && 'password' in inUser)) {
-			res.status(400).send({ message: "Invalid input data for route" });
-			return;
-		}
-		let dbUser = findUserInDB(inUser, res);
-		if (dbUser) return;
+    if (await bcrypt.compare(inUser.password, dbUser.password)) {
+      inUser.id = dbUser.id;
+      inUser.password = dbUser.password;
+      let token = tokenAuth.create(inUser);
+      res.status(200).send({ token: token, user: dbUser });
+    } else res.status(409).send({ message: "Unsuccessful login" });
+  };
 
-		if (await bcrypt.compare(inUser.password, dbUser.password)) {
-			inUser.id = dbUser.id;
-			inUser.password = dbUser.password;
-			let token = tokenAuth.create(inUser);
-			res.status(200).send({ token: token, user: dbUser });
-		} else res.status(409).send({ message: "Unsuccessful login" });
-	};
+  const deleteUser = async (req, res) => {
+    let inUser = req.body;
+    inUser.userName = inUser.userName ?? "";
+    inUser.email = inUser.email ?? "";
 
-	const deleteUser = async (req, res) => {
-		let inUser = req.body;
-		inUser.userName = inUser.userName ?? "";
-		inUser.email = inUser.email ?? "";
+    if (userPropCheck(inUser, res)) return;
 
-		if (userPropCheck(inUser, res)) return;
+    let dbUser = findUserInDB(inUser, res);
+    if (dbUser) return;
+    try {
+      if (await bcrypt.compare(inUser.password, dbUser.password)) {
+        await User.destroy({
+          where: {
+            userName: userName,
+            [Op.or]: {
+              email: email,
+            },
+          },
+        });
+        res
+          .status(200)
+          .send({ message: `Successfuly deleted user ${userName}` });
+      }
+    } catch (e) {
+      res.status(409).send({ message: `Unable to delete user ${userName}` });
+    }
+  };
 
-		try {
-			let dbUser = findUserInDB(inUser, res);
-			if (dbUser) return;
+  const getUser = async (req, res) => {
+    let inUser = req.body;
+    let isGood = false;
+    ["email", "userName"].forEach((el) => {
+      if (!(el in inUser)) inUser[el] = "";
+      else isGood = true;
+    });
+    if (!isGood) {
+      res.status(400).send({ message: "Invalid input data for route" });
+      return;
+    }
 
-			if (await bcrypt.compare(inUser.password, dbUser.password)) {
-				await User.destroy({
-					where: {
-						userName: userName,
-						[Op.or]: {
-							email: email,
-						},
-					},
-				});
-				res.status(200)
-					.send({ message: `Successfuly deleted user ${userName}` });
-			}
-		} catch (e) {
-			res
-				.status(409)
-				.send({ message: `Unable to delete user ${userName}` });
-		}
-	};
+    let dbUser = findUserInDB(inUser, res);
+    if (dbUser) return;
 
+    res.status(200).send(dbUser);
+  };
 
-	const getUser = async (req, res) => {
-		let inUser = req.body;
-		let isGood = false;
-		['email', 'userName'].forEach(el => {
-			if (!(el in inUser)) inUser[el] = '';
-			else isGood = true;
-		});
-		if (!isGood) {
-			res.status(400).send({ message: "Invalid input data for route" });
-			return;
-		}
+  const getAllUsers = async (req, res) => {
+    let allUser = await User.findAll({
+      attributes: ["userName"],
+    });
+    res.status(200).send({ allUsers: allUser.map((el) => el.userName) });
+  };
 
-		let dbUser = findUserInDB(inUser, res);
-		if (dbUser) return;
-
-		res.status(200).send(dbUser);
-	};
-
-	const getAllUsers = async (req, res) => {
-		let allUser = await User.findAll({
-			attributes: ['userName']
-		});
-		res.status(200).send({ allUsers: allUser.map(el => el.userName) });
-	}
-
-
-	return [
-		register,
-		login,
-		deleteUser,
-		getUser,
-		getAllUsers
-	].reduce((obj, curr) => ({ ...obj, [curr.name]: routeWrappers.serverErrorWrap(curr) }), {});
+  return [register, login, deleteUser, getUser, getAllUsers].reduce(
+    (obj, curr) => ({
+      ...obj,
+      [curr.name]: routeWrappers.serverErrorWrap(curr),
+    }),
+    {}
+  );
 })();
 
 module.exports = userController;
